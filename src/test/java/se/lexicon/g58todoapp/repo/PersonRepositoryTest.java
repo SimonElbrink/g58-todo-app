659c9be68b2e20917bff278c4278337b12677e22
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2026-01-27 14:50:32 +0100

    Add test placeholders for `TodoRepository` and `PersonRepository`

---
+++ src/test/java/se/lexicon/g58todoapp/repo/PersonRepositoryTest.java
@@ -0,0 +1,13 @@
+package se.lexicon.g58todoapp.repo;
+
+import lombok.Data;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class PersonRepositoryTest {
+
+    //TODO TESTS HERE
+
+}
---
+++ src/test/java/se/lexicon/g58todoapp/repo/TodoRepositoryTest.java
@@ -0,0 +1,18 @@
+package se.lexicon.g58todoapp.repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class TodoRepositoryTest {
+
+    //TODO TESTS HERE
+
+
+    @Test
+    void name() {
+
+    }
+}
