60140cbd790fb8a60b55527e294d7e4274e8c212
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2026-02-04 10:48:23 +0100

    BUG AWARE! Implement `TodoDto`, `TodoService` with `create` method, and extend `TodoController` with a POST endpoint for creating todos.

--- src/main/java/se/lexicon/g58todoapp/controller/TodoController.java
+++ src/main/java/se/lexicon/g58todoapp/controller/TodoController.java
@@ -1,6 +1,32 @@
 package se.lexicon.g58todoapp.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.lexicon.g58todoapp.dto.TodoDto;
+import se.lexicon.g58todoapp.service.PersonService;
+import se.lexicon.g58todoapp.service.TodoService;
+
+@RequestMapping("api/todos")
+@RestController
 public class TodoController {
 
-    //TODO: Implement basic CRUD operations for Todo entity
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<?> createTodo(TodoDto todoDto){
+
+        todoService.create(todoDto);
+
+        return ResponseEntity.status(201).build();
+
+    }
+
+
 }
